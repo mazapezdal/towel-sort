1a6b2d4b428548c8c3bc14cc383a5f51ea10a4d4
Vlad <105743454+mazapezdal@users.noreply.github.com>
2022-08-11 10:03:08 +0300

    Update index.js
    feat: #next

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 module.exports = function towelSort (matrix) {
     let arr = [];
     for(i=0; i<matrix.length; i++){
+        if(matrix= []){
+            return arr;
+        }
     if (i % 2 != 0) {
-        arr.push(matrix[i].reverse().flat());
+        arr.push(matrix[i].reverse());
     } else {
-        arr.push(matrix[i].flat());
+        arr.push(matrix[i]);
     };
     };    
-  return arr;
+ return arr.flat();
 }
